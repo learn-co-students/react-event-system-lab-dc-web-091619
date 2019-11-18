efd77bef96de8a8a68fc178760488e742e110e83
nihaprezz <sergio13prez@gmail.com>
2019-11-18 16:24:33 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,37 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+// const eyesOnMe = () => {
+    
+//     focusedOn = () => {
+//         console.log('Good!')
+//     }
+
+
+
+//     return (
+//         <div> 
+//             <button onFocus = {this.focusedOn} />  this is not defined? why???     
+//         </div>
+//     )
+// }
+
+class EyesOnMe extends React.Component{
+
+    focusedOn = () => {
+        console.log('Good!')
+    }
+
+    focusedBlur = () => {
+        console.log('Hey! Eyes on me!' )
+    }
+
+    render(){
+        return (
+            <button onFocus = {this.focusedOn}
+            onBlur = {this.focusedBlur}/>
+        )
+    }
+}
+
+export default EyesOnMe
